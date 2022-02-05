b4f8db63025d50cd7067d2664e94b5b907455667
dgpgean <52607904+dgpgean@users.noreply.github.com>
2022-02-05 14:52:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+;(function () {
+  /*_______Abre e fecha menu_______*/
+  const nav = document.querySelector('#header nav')
+  const toggle = document.querySelectorAll('nav .toggle')
+  for (const element of toggle) {
+    element.addEventListener('click', function () {
+      nav.classList.toggle('show')
+    })
+  }
+
+  /*_______Esconder menu com clique em um link do menu_______*/ 
+  const links = document.querySelectorAll('nav ul li a')
+  for (const link of links) {
+    link.addEventListener('click', function () {
+      nav.classList.remove('show')
+    })
+  }
+
+
+  /*_______adiciona sombra no header quando der scrool_______*/
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+  function changeHeadrWhenScroll() {
+    if(window.scrollY >= navHeight){
+      header.classList.add('scroll')
+    }else{
+      header.classList.remove('scroll')
+    }
+    }
+
+    /*_______Carrossel swipper_______*/
+
+    const swiper = new Swiper('.swiper', {
+      slidesPerView:1,
+      pagination:{
+        el:'.swiper-pagination'
+      },
+      mousewheel: true,
+      Keyboard: true,
+      breakpoints:{
+        767:{
+          slidesPerView:2,
+          setWrapperSize: true
+        }
+      }
+    });
+
+    /*_______Scrool reveal_______*/
+    const scrollReveal = ScrollReveal({
+      origin:'top',
+      distance:'30px',
+      duration: 700,
+      reset:true
+    })
+
+    scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer.social
+    `,{interval:100})
+
+
+    /*_______Button Up_______*/
+    const backToTopButton = document.querySelector('.back-to-top')
+    function backToTop(){
+      if(window.scrollY >= 560){
+        backToTopButton.classList.add('show')
+      }
+      else{
+        backToTopButton.classList.remove('show')
+      }
+    }
+  
+    
+    /*_______Marcação do Menu_______*/
+    const sections = document.querySelectorAll('main section[id]')
+    function activateMenuAtCurrentSection(){
+      const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+      for(const section of sections){
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+        
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint<= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd){
+          document.querySelector('nav ul li a[href*='+sectionId+']').classList.add('active')
+        }else{
+          document.querySelector('nav ul li a[href*='+sectionId+']').classList.remove('active')
+        }
+
+      }
+
+    }
+    
+
+    /*_______scrool_______*/
+    window.addEventListener('scroll', function () {
+      changeHeadrWhenScroll()
+      backToTop()
+      activateMenuAtCurrentSection()
+    }
+    )
+
+
+})()
